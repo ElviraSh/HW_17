3741de18773b30d13ddd49b073df1d68a4d0cf47
Эльвира Шалафаева < >
2020-01-18 20:51:35 +0300

    flowcharts

--- src/InputOutputTextFile.java
+++ src/InputOutputTextFile.java
@@ -6,16 +6,22 @@ public class InputOutputTextFile {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("input number: ");
-        int number = sc.nextInt();
+        System.out.println("Введите значение n: ");
+        int n = sc.nextInt();//считываем значение с клавиатуры
 
-        for (int i = 2; i <= number; i++) {
-            for (int j = 2; j <= i; j++) {
-                if (number % j == 0) {
-                    System.out.print(j + " ");
-                    number = number / j;
+        if ( n < 0) { System.out.println("Введите натуральное значение!"); }//если значение меньше 0, число не натуральное
+        else {
+            System.out.println("Разложение числа " + n + " на простые множители:");
+            for (int i = 2; i <= n; i++) {
+                for (int j = 2; j <= i; j++) {
+                    if (n % j == 0) { // получаем остаток от деления n на j, если он = 0, он - простой делитель
+                        System.out.println(j);
+                        n = n / j;
+
+                    }
+
+
                 }
-
             }
         }
     }
--- src/Multiplicity.java
+++ src/Multiplicity.java
@@ -10,28 +10,39 @@ public class Multiplicity {
         System.out.print("Введите значение n");
         int n = in.nextInt(); //вводим значение n
 
-        System.out.print("Введите значение a");
-        int a = in.nextInt(); //вводим значение a
 
-        in.close();
 
-        int div1 = n/1000; // получаем первую цифру
-        int div2 = n/100%10; // получаем вторую цифру
-        int div3 = n/10%10; // получаем третью цифру
-        int div4 = n%10; // получаем четвертую цифру
+        int del1 = n/1000; // получаем первую цифру
+        int del2 = n/100%10; // получаем вторую цифру
+        int del3 = n/10%10; // получаем третью цифру
+        int del4 = n%10; // получаем четвертую цифру
 
-        int multiplication = div1 * div2 * div3 * div4; // произведение всех цифр числа
+        if ((del1 == 0) | ((del1 ==0) & (del2 ==0)) | ((del1 == 0) & (del2 == 0) & (del3 ==0))){ //если перая и вторая цифра = 0, то ч
+            System.out.println("Введите четырехзначное число");
 
-        if (multiplication % 3 == 0) { // если при делении произведения на 3 нет остатка
-            System.out.println("Произведение цифр числа " + n + " кратно 3");
-        } else { //иначе
-            System.out.println("Произведение цифр числа " + n + " не кратно 3");
         }
+        else {
 
-        if (multiplication % a == 0) { // если при делении произведения на а нет остатка
-            System.out.println("Произведение цифр числа " + n + " кратно " + a);
-        } else{
-            System.out.println("Произведение цифр числа " + n + "  не кратно " + a);
+
+
+            int multiplication = del1 * del2 * del3 * del4; // произведение всех цифр числа
+
+            if (multiplication % 3 == 0) { // если при делении произведения на 3 нет остатка
+                System.out.println("Произведение цифр числа " + n + " кратно 3");
+            } else { //иначе
+                System.out.println("Произведение цифр числа " + n + " не кратно 3");
+            }
+
+            System.out.print("Введите значение a");
+            int a = in.nextInt(); //вводим значение a
+
+            in.close();
+
+            if (multiplication % a == 0) { // если при делении произведения на а нет остатка
+                System.out.println("Произведение цифр числа " + n + " кратно " + a);
+            } else {
+                System.out.println("Произведение цифр числа " + n + "  не кратно " + a);
+            }
         }
     }
 }
